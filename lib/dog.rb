004dae610910de05057fea8fa0a820647cd32a8f
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-07 00:52:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,15 +3,19 @@ class Dog
   @@all = []
   def initialize(name)
     @name = name 
- @@all << self
+ @@all << name
+ save
   end
   def self.all 
-    @@all 
+   save
   end
   def self.print_all
     @@all.map{|x|puts x}
   end
   def self.clear_all
    @@all.clear
+end
+def save
+  @@all << self
 end
 end
